b4b70155356ef795d50c0d244853245a3ebd2f8b
isMbumbu <andrewambuka9@gmail.com>
2024-10-05 23:50:38 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+let cats=["Milo","Otis","Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function appendCat(name) {
+    return [...cats, name]; // Return a new array
+}
+function prependCat(name){
+    return [name , ...cats];
+}
+function removeLastCat() {
+    return cats.slice(0, -1); // Return a new array without the last cat
+}
+
+function removeFirstCat() {
+    return cats.slice(1); // Return a new array without the first cat
+}
